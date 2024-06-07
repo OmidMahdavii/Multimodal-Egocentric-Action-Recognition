21b3a3787a73c3e69b9787163fe4fa1d67aa00cc
Omid Mahdavi <s.omidmahdavi@yahoo.com>
2024-06-07 11:29:22 +0200

    Feed all clips

--- models/FinalClassifier.py
+++ models/FinalClassifier.py
@@ -1,28 +1,30 @@
 from torch import nn
 
 
-class Classifier(nn.Module):
-    def __init__(self, num_classes):
+class CNN(nn.Module):
+    def __init__(self, num_classes, num_clips):
         super().__init__()
         """
         [TODO]: the classifier should be implemented by the students and different variations of it can be tested
         in order to understand which is the most performing one """
 
-        self.cnn = nn.Sequential(
-            # Input shape: (1, 1024)
-            nn.Conv1d(in_channels=1, out_channels=2, kernel_size=6, stride=4, padding=1),
-            nn.BatchNorm1d(2),
+        self.model = nn.Sequential(
+            # Input shape: (num_clips, 1024)
+            nn.Conv1d(in_channels=num_clips, out_channels=num_clips*2, kernel_size=6, stride=4, padding=1),
+            nn.BatchNorm1d(num_clips*2),
             nn.ReLU(),
-            # Shape: (2, 256)
-            nn.Conv1d(in_channels=2, out_channels=4, kernel_size=6, stride=4, padding=1),
-            nn.BatchNorm1d(4),
+            # Shape: (num_clips*2, 256)
+            nn.Conv1d(in_channels=num_clips*2, out_channels=num_clips*4, kernel_size=6, stride=4, padding=1),
+            nn.BatchNorm1d(num_clips*4),
             nn.ReLU(),
-            # Shape: (4, 64)
-            nn.Conv1d(in_channels=4, out_channels=8, kernel_size=6, stride=4, padding=1),
-            nn.BatchNorm1d(8),
+            # Shape: (num_clips*4, 64)
+            nn.Conv1d(in_channels=num_clips*4, out_channels=num_clips*8, kernel_size=6, stride=4, padding=1),
+            nn.BatchNorm1d(num_clips*8),
             nn.ReLU(),
-            # Shape: (4, 16)
-            nn.Conv1d(in_channels=8, out_channels=num_classes, kernel_size=16, stride=1, padding=0),
+            # Shape: (num_clips*8, 16)
+            # nn.Conv1d(in_channels=8, out_channels=num_classes, kernel_size=16, stride=1, padding=0),
+            nn.Conv1d(in_channels=num_clips*8, out_channels=num_clips*16, kernel_size=16, stride=1, padding=0),
+            nn.Linear(num_clips*16, num_classes),
             # Shape: (num_classes, 1)
             nn.Sigmoid()
         )
@@ -30,6 +32,7 @@ class Classifier(nn.Module):
         
 
     def forward(self, x):
-        x = x.unsqueeze(1)
-        y = self.cnn(x)
-        return y.squeeze(), {}
+        # x = x.unsqueeze(1)
+        # y = self.cnn(x)
+        # return y.squeeze(), {}
+        return self.model(x), {}
